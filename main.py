882c401d795b8d13a71b95e485784d0ce5240e74
gareins <ozbolt.menegatti@gmail.com>
2016-01-03 00:28:04 +0100

    Finished network analysis, most mutations are added. Outside signals working.
    TODO soon:
    - Analyze ode output and mark it
    - Guided mutation
    - Adding/removing parameters
    - Add error handling
    - Finalize the shit out of it.

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from enum import Enum
 import random
-
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-# Change factor
-NO_CHG_TYPE = 0.9
-CHG_FACTOR = 0.5
-N_PROTEINS = 3
-ADD_RM_PROTEIN_EXPRESSION = 0.1
 
 class Type(Enum):
     linear = 1
@@ -19,19 +13,32 @@ class Type(Enum):
 D_TYPES = (Type.linear, Type.linear, Type.linear, Type.linear, Type.encime, Type.encime, Type.active)
 ARP_TYPES = (Type.linear, Type.linear, Type.encime)
 
+# Change factor
+NO_CHG_TYPE = 0.9
+CHG_FACTOR = 0.5
+N_PROTEINS = 3
+ADD_RM_PROTEIN_EXPRESSION = 0.1
+FREQUENCY = 50
+OUTER_DIFF = 3
+DT = 5
+
+
 class Expression:
     def __init__(self):
         self._typ = Type.linear
         self._factor = 1
         self._limit = 0
-        self._othr = None
+        self._other = None
 
-    def set_othr(self, othr):
-        self._othr = othr
+    def set_other(self, other):
+        self._other = other
 
-    def mutate(self, othr, new_type):
+    def get_other(self):
+        return self._other
+
+    def mutate(self, other, new_type):
         if random.random() < NO_CHG_TYPE:
-            if CHG_FACTOR or self.Type != self.encime:
+            if CHG_FACTOR or self._typ != Type.encime:
                 self._factor *= random.random() * 2
             else:
                 self._limit += (random.random() * 3 - 1)
@@ -39,7 +46,7 @@ class Expression:
                     self._limit = 0
         else:
             self._typ = new_type
-            self._othr = othr
+            self._other = other
             self._factor = 1
             self._limit = 0
 
@@ -50,88 +57,96 @@ class Expression:
 
         return new_type
 
+
 class ActRep(Expression):
     def calc_diff_out(self, prot):
         diff = 0
         if self._typ == Type.linear:
             diff = self._factor
         elif self._typ == Type.encime:
-            ra = 1 if self._othr.val - prot.val < 0 else 0
+            ra = 1 if self._other.val - prot.val < 0 else 0
             ra = ra if self._factor > 0 else 1 - ra
             diff = self._factor * ra
-        else:
-            print("BAD_332")
+
         return diff
 
-    def mutate(self, othr):
+    def mutate(self, other, new_type=None):
         new_type = super(ActRep, self).get_random_typ(ARP_TYPES)
-        super(ActRep, self).mutate(othr, new_type)
+        super(ActRep, self).mutate(other, new_type)
+
 
 class Activator(ActRep):
-    def calc_diff(self, prot, proteins):
-        prot.diff += self.calc_diff_out(prot)
+    def calc_diff(self, protein, _):
+        protein.diff += self.calc_diff_out(protein)
+
+
 class Repressor(ActRep):
-    def calc_diff(self, prot, proteins):
-        prot.diff -= self.calc_diff_out(prot)
+    def calc_diff(self, protein, _):
+        protein.diff -= self.calc_diff_out(protein)
+
 
 class Degradation(Expression):
-    def calc_diff(self, prot, proteins):
+    def calc_diff(self, prot, _):
         if self._typ == Type.linear:
             diff = prot.val * self._factor
         elif self._typ == Type.encime:
             diff = self._factor * prot.val / (self._limit + prot.val)
         else:
-            diff = self._factor * prot.val * self._othr.val
+            diff = self._factor * prot.val * self._other.val
         prot.diff -= diff
 
-    def mutate(self, othr):
+    def mutate(self, other, new_type=None):
         new_type = super(Degradation, self).get_random_typ(D_TYPES)
-        super(Degradation, self).mutate(othr, new_type)
+        super(Degradation, self).mutate(other, new_type)
+
 
 class ProteinModification(Expression):
-    def calc_diff(self, prot, proteins):
+    def calc_diff(self, protein, _):
+        diff = 0
         if self._typ == Type.linear:
-            diff = self._factor * prot.val
+            diff = self._factor * protein.val
         elif self._typ == Type.encime:
-            diff = self._factor * prot.val / (self._limit + prot.val)
-        else:
-            print("BAD_123")
-        prot.diff -= diff
-        self._othr.diff += diff
+            diff = self._factor * protein.val / (self._limit + protein.val)
 
-    def mutate(self, othr):
+        protein.diff -= diff
+        self._other.diff += diff
+
+    def mutate(self, other, new_type=None):
         new_type = super(ProteinModification, self).get_random_typ(ARP_TYPES)
-        super(ProteinModification, self).mutate(othr, new_type)
+        super(ProteinModification, self).mutate(other, new_type)
 
 ###############################################
 # End of expressions, now Protein and Network #
 ###############################################
 
+
 class Protein:
     def __init__(self):
         self.val = 1
         self.diff = 0
-
         self.expressions = {
             "ACT": Activator(),
             "REP": Repressor(),
             "DEG": Degradation(),
         }
 
+    # A mutation that adds ProteinMutation expression to protein
     def add_expression_pm(self, proteins):
         for prot in random.sample(proteins, len(proteins)):
             if prot not in self.expressions and prot != self:
                 new_expr = ProteinModification()
-                new_expr.set_othr(prot)
+                new_expr.set_other(prot)
                 self.expressions[prot] = new_expr
                 break
 
+    # A mutation that removes ProteinMutation expression to protein
     def rm_expression_pm(self):
         for k in random.sample(list(self.expressions.keys()), len(self.expressions)):
             if k not in ("ACT", "REP", "DEG") and self.expressions[k] is not None:
                 del self.expressions[k]
                 break
 
+    # Extually mutating this protein
     def mutate(self, proteins, k=None):
         prot = random.choice(proteins)
         while prot == self:
@@ -149,52 +164,92 @@ class Protein:
                 k = random.choice(list(self.expressions.keys()))
             self.expressions[k].mutate(prot)
 
-    def remove_protein(self, prot, proteins):
+    def remove_protein(self, protein, proteins):
         chk = {"ACT": Activator(), "DEG": Degradation(), "REP": Repressor}
         for k, e in chk.items():
-            if self.expressions[k]._othr == prot:
+            if self.expressions[k].get_other == protein:
                 self.expressions[k] = e
                 self.expressions[k].mutate(proteins)
 
-        del self.expressions[prot]
+        del self.expressions[protein]
+
 
 class Network:
+    """
+    Contains a couple of proteins that are connected with
+    some expressions (degradation, activation,...).
+    Also includes utilities to simulate the network and analysis.
+    TODO: add analysis and mutation
+    """
     def __init__(self):
         self.proteins = [Protein() for _ in range(N_PROTEINS)]
         self.mutation_rate = 5
         self.graph = None
+        self.time = None
 
     def mutate(self):
+        # Select random protein and mutate "mutation_rate" number of times
         for i in range(self.mutation_rate):
             p = random.choice(self.proteins)
             p.mutate(self.proteins)
 
     def diff_closure(self):
+        first_in = FREQUENCY / 2
+        second_in = FREQUENCY
+        k = 4 * OUTER_DIFF / (DT * DT)
+
         # Defining calc diff closure function
         # for use in odeint. For every protein
-        def calc_diff(X, t):
+        def calc_diff(x, t):
+            # Copy values into protein
             for i, p in enumerate(self.proteins):
-                p.val = X[i]
+                p.val = x[i]
                 p.diff = 0
 
+            # Calculate diffs from all expressions in all proteins
             df = []
             for p in self.proteins:
                 for e in p.expressions.values():
                     e.calc_diff(p, self.proteins)
                 df.append(p.diff)
+
+            # Handle output signal with triangle DIFF
+            # This has to be done with "triangle" in order for ODE
+            # to converge. DIFF function looks like this:
+            """
+            |       Integral is OUTER_DIFF = total change in protein in this time
+            |       k is steepness of this function
+            |   /\  DT is how much time the protein will be changing in value.
+            |  /  \
+            | /    \
+            |/      \
+            +--------DT---->
+            """
+            nonlocal first_in, second_in, k
+            if first_in < t < first_in + DT:
+                lt = (t - first_in)
+                diff = lt * k if lt < DT / 2 else -lt * k + k * DT
+                df[0] += diff
+            elif t > first_in + DT:
+                first_in += FREQUENCY
+
+            if second_in < t < second_in + DT:
+                lt = (t - second_in)
+                diff = lt * k if lt < DT / 2 else -lt * k + k * DT
+                df[1] += diff
+            elif t > second_in + DT:
+                second_in += FREQUENCY
+
             return df
 
         # return the closure
         return calc_diff
 
     def analyze_me(self):
-        cls = self.diff_closure()
-        print(cls([1, 1, 1], 1))
-
         Network.current = self
         init = [p.val for p in self.proteins]
-        self.time = np.linspace(0, 100, num=3000)
-        self.graph = odeint(self.diff_closure(), init, self.time)
+        self.time = np.linspace(0, 200, num=16384)
+        self.graph = odeint(self.diff_closure(), init, self.time, hmin=0.0001, hmax=1)
 
     def plot_me(self):
         a = self.graph[:, 0]
@@ -213,18 +268,3 @@ if __name__ == "__main__":
         ntw.analyze_me()
         ntw.plot_me()
         ntw.mutate()
-
-
-#     def eq(X, t):
-#         x, y = X
-#         return [x - y - np.exp(t), x + y + 2 * np.exp(t)]
-# 
-#     init = [-1.0, -1.0]
-#     t = np.linspace(0, 4, num=50)
-#     X = odeint(eq, init, t)
-# 
-#     x = X[:, 0]
-#     y = X[:, 1]
-# 
-#     plt.plot(t, x, 'k--')
-#     plt.plot(t, y, 'k:')
