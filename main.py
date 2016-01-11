e254605f76e7d9ac5b7a055ff1f2c71d33db9ba2
gareins <ozbolt.menegatti@gmail.com>
2016-01-11 21:46:42 +0100

    Adding "advanced" fitness function.
    Also some PEP violations corrected.
    
    TODO: I will clean up the code and document it later :)
    Anyone is welcome to do so now.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from enum import Enum
+import os
+import pickle
+import random
+import shutil
+import string
 from copy import deepcopy
+from enum import Enum
+from math import floor
 
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
-import matplotlib.pyplot as plt
-import os, sys, shutil, string, random, pickle
 
 CTR = 0
 BEST_EVAL = 99999999.99
 LOAD_FILE_LOC = ""
 LOAD_FILE = False
+
 
 class Type(Enum):
     linear = 1
@@ -20,30 +26,71 @@ D_TYPES = (Type.linear, Type.linear, Type.linear, Type.linear, Type.encime, Type
 ARP_TYPES = (Type.linear, Type.linear, Type.encime)
 
 # Change factor
-NO_CHG_TYPE = 0.9
+NO_CHG_TYPE = 0.8
 CHG_FACTOR = 0.5
 N_PROTEINS = 4
 ADD_RM_PROTEIN_EXPRESSION = 0.05
 ADD_RM_PROTEIN = 0.02
+
 PROTEIN_MUTATION_DEFAULT = 2
 GENERATION_MUTATION_RATE = 4
-RUN_TIME = 200
-N_STEPS = 5e4
+
+RUN_TIME = 400
+N_STEPS = int(2e4)
 
 N_NETS_BEFORE = 20
 N_NETS_AFTER = 4
 
-FREQUENCY = 100
-OUTER_DIFF = 3
-DT = 5
-
 ON_VAL = 3
 OFF_VAL = 0
-WORST_EVAL = 10
-WORST_EVAL_ABS = WORST_EVAL * N_STEPS
+WORST_EVAL_SIMPLE = 10
 
 BORDER_VALUE = 30
+FREQUENCY = 100
+DT = 5
+OUTER_DIFF = 3
 DIFF_BETWEEN_STATES = 2
+WORST_EVAL_ADV = 1000
+
+UP_PTS = []
+DN_PTS = []
+UP_DN_PTS_INITIALIZED = False
+
+WORST_EVAL = WORST_EVAL_ADV
+WORST_EVAL_ABS = WORST_EVAL * N_STEPS
+
+
+def init_adv_eval_pts():
+    global UP_PTS, DN_PTS, UP_DN_PTS_INITIALIZED
+    if not UP_DN_PTS_INITIALIZED:
+        UP_DN_PTS_INITIALIZED = True
+    else:
+        return
+
+    f = FREQUENCY
+    up_pt1, up_pt2 = 0.75 * f, f - 1
+    dn_pt1, dn_pt2 = 1.25 * f, 1.5 * f - 1
+    ratio = N_STEPS / RUN_TIME
+
+    while True:
+        if up_pt1 < RUN_TIME:
+            UP_PTS += [floor(up_pt1 * ratio)]
+
+        if up_pt2 < RUN_TIME:
+            UP_PTS += [floor(up_pt2 * ratio)]
+
+        if dn_pt1 < RUN_TIME:
+            DN_PTS += [floor(dn_pt1 * ratio)]
+
+        if dn_pt2 < RUN_TIME:
+            DN_PTS += [floor(dn_pt2 * ratio)]
+
+        else:
+            break
+
+        up_pt1, up_pt2 = up_pt1 + f, up_pt2 + f
+        dn_pt1, dn_pt2 = dn_pt1 + f, dn_pt2 + f
+
 
 class Expression:
     def __init__(self):
@@ -168,7 +215,7 @@ class Protein:
                 self.expressions.pop(k)
                 break
 
-    # Extually mutating this protein
+    # Actually mutating this protein
     def mutate(self, proteins, k=None):
         prot = random.choice(proteins)
         while prot == self:
@@ -211,6 +258,7 @@ class Network:
 
         self.time = np.linspace(0, RUN_TIME, num=N_STEPS)
         self.ideal = [OFF_VAL if (t % FREQUENCY) < FREQUENCY / 2 else ON_VAL for t in self.time]
+        self.adv_pts = []
 
     def mutate(self):
         if random.random() < ADD_RM_PROTEIN:
@@ -286,13 +334,14 @@ class Network:
     def run_simulation(self):
         init = [p.val for p in self.proteins]
 
+        # noinspection PyBroadException
         try:
-            self.graph, info = odeint(self.diff_closure(), init, self.time, hmin=0.0001, hmax=1, printmessg=False, full_output=True)
+            self.graph, info = odeint(self.diff_closure(), init, self.time, hmax=1, printmessg=False, full_output=True)
             if info['message'] != 'Integration successful.':
                 return WORST_EVAL + 1
 
             # HERE should go the evaluation of the result
-            return self.evaluate_simple()
+            return self.evaluate_adv()
 
         except Exception:
             return WORST_EVAL + 2
@@ -302,28 +351,55 @@ class Network:
             for j in range(len(self.time)):
                 if not -1 < self.graph[j, p] < BORDER_VALUE:
                     return 1
-        return -1				
-			
+        return -1
+
     def evaluate_simple(self):
         if self.eval_border_check() > 0:
             return WORST_EVAL - 1
-	
-        eval = WORST_EVAL_ABS
+
+        fitness = WORST_EVAL_ABS
         for p in range(2, len(self.proteins)):
-            eval = min(eval, sum((self.graph[i, p] - self.ideal[i]) ** 2 for i in range(len(self.time))))
-		
-        return eval / N_STEPS
-		
+            fitness = min(fitness, sum((self.graph[i, p] - self.ideal[i]) ** 2 for i in range(len(self.time))))
+
+        return fitness / N_STEPS
+
     def evaluate_adv(self):
         if self.eval_border_check() > 0:
-            return WORST_EVAL - 1
-		
-		
-        return eval
+            return WORST_EVAL + 1
+
+        init_adv_eval_pts()
+
+        fitness = WORST_EVAL + 1
+        self.adv_pts = []
+
+        for p in range(2, len(self.proteins)):
+            up_vals = [self.graph[i, p] for i in UP_PTS]
+            dn_vals = [self.graph[i, p] for i in DN_PTS]
+
+            diff_up = max(up_vals) - min(up_vals)
+            diff_dn = max(dn_vals) - min(dn_vals)
+
+            diff_up_dn = sum(up_vals) / len(UP_PTS) - sum(dn_vals) / len(DN_PTS)
+            if diff_up_dn > DIFF_BETWEEN_STATES:
+                diff_up_dn = 0
+            elif diff_up_dn < DIFF_BETWEEN_STATES / 10:
+                diff_up_dn = WORST_EVAL * (1 - diff_up_dn)
+            else:
+                diff_up_dn = DIFF_BETWEEN_STATES - diff_up_dn
+
+            e = (diff_up + diff_dn + diff_up_dn)
+            if e < fitness:
+                fitness = e
+                self.adv_pts = up_vals + dn_vals
+
+        return fitness
 
     def plot_me(self):
         plt.close()
-        plt.plot(self.time, self.ideal, label='Ideal')
+
+        plt.plot([self.time[i] for i in UP_PTS + DN_PTS], self.adv_pts, 'ro', label='Evaluated points')
+        # plt.plot(self.time, self.ideal, label='Ideal')
+
         for i, _ in enumerate(self.proteins):
             g = self.graph[:, i]
             plt.plot(self.time, g, label='Protein ' + str(i + 1))
@@ -331,19 +407,20 @@ class Network:
         plt.legend(bbox_to_anchor=(0., 1.05, 1., .102), loc=3, ncol=2, mode="expand")
         global CTR
         plt.title('Plot Nr: ' + str(CTR))
-        plt.savefig('./latest/npmp_' + str(CTR // 100)  + str((CTR // 10) % 10) + str(CTR % 10) + '.png', bbox_inches='tight')
-		
+        plt.savefig('./latest/img_' + "{:05d}".format(CTR) + '.png', bbox_inches='tight')
+
     def save_network(self):
-        file_Name = './latest/npmp_networks_' + str(CTR // 100)  + str((CTR // 10) % 10) + str(CTR % 10) + '.db'
-        fileObject = open(file_Name,'wb') 
-        pickle.dump(self,fileObject)   
-        fileObject.close()
-		
-def load_network(file_Name):
-    fileObject = open(file_Name,'rb')  
-    n = pickle.load(fileObject) 
-    fileObject.close()
-    return n
+        file_name = './latest/net_' + "{:05d}".format(CTR) + '.p'
+        f = open(file_name, 'wb')
+        pickle.dump(self, f)
+        f.close()
+
+
+def load_network(file_name):
+    with open(file_name, 'rb') as f:
+        n = pickle.load(f)
+        return n
+
 
 def mutation_stuff():
     networks = []
@@ -362,11 +439,11 @@ def mutation_stuff():
         for n in networks:
             analysis.append((n, n.run_simulation()))
 
-        #load here network so run_simulation don't mess it up!
+        # Load here network so run_simulation don't mess it up!
         global LOAD_FILE, LOAD_FILE_LOC
         if LOAD_FILE:
             n2 = load_network(LOAD_FILE_LOC)
-            analysis.append((n2, n2.evaluate_simple())) #change to advanced
+            analysis.append((n2, n2.evaluate_simple()))  # change to advanced
             LOAD_FILE = False
 
         # Sort analysis
@@ -376,12 +453,15 @@ def mutation_stuff():
         best_analysis = analysis[0]
         avg_analysis = sum(a[1] for a in analysis[:N_NETS_AFTER]) / N_NETS_AFTER
         print("Generation %d: BEST: %f, AVERAGE: %f" % (CTR, best_analysis[1], avg_analysis))
-        best_analysis[0].plot_me() #maybe also this generate only if results are better ??
-		#Save the best network into a file only if they are better results than before
+        best_analysis[0].plot_me()  # maybe also this generate only if results are better ??
+
+        # Save the best network into a file only if they are better results than before
         if BEST_EVAL > best_analysis[1]:
             best_analysis[0].save_network()
             BEST_EVAL = best_analysis[1]
-        CTR += 1 #?? just incase we will move plot saving only on best generations 
+
+        CTR += 1  # ?? just in case we will move plot saving only on best generations
+
         # Generate new mutations
         networks = []
         good_old_analysis = analysis[:N_NETS_AFTER]
@@ -394,19 +474,22 @@ def mutation_stuff():
                 n.mutate()
                 networks.append(n)
 
+
 def prompt_load_network():
-    loc =input('Load best network file loc: ')
+    loc = input('Load best network file loc: ')
     if os.path.isfile(loc):
         global LOAD_FILE, LOAD_FILE_LOC
         LOAD_FILE = True
         LOAD_FILE_LOC = loc
-				
+                
 if __name__ == "__main__":
-    #moving olg files
+    # moving old files
     if os.path.exists("./latest"):
         rand = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(10))
-        shutil.move("./latest/.","./"+rand+"/")
+        shutil.move("./latest", "./" + rand)
+
     os.makedirs("./latest")
-	#generation of networks load'n stuff
+    # generation of networks load'n stuff
+
     prompt_load_network()
     mutation_stuff()
